c2508e5ad998b56a508510fbb84bd27b7d88f17b
Leonardo Francisco Da Silva <leozinho24680@gmail.com>
2025-11-05 10:49:19 -0300

    iniciando trilha de estudos "operadores"

---
+++ sintaxe-java/src/edu/leonardo/operadores/concatenacao.java
@@ -0,0 +1,19 @@
+package edu.leonardo.operadores;
+
+public class concatenacao {
+    public static void main(String[] args) {
+
+    // Operador de Concatenação
+
+    // Concatenando duas Strings
+    String nomeCompleto = "Linguagem " + "Java";
+    System.out.println(nomeCompleto);
+
+
+// Concatenando String com número
+    String nome = "Leonardo";
+    int idade = 15;
+    System.out.println("Meu nome é " + nome + "e tenho " + idade + "anos.");
+
+    }
+}
---
+++ sintaxe-java/src/edu/leonardo/operadores/operadores_aritmeticos.java
@@ -0,0 +1,25 @@
+package edu.leonardo.operadores;
+public class operadores_aritmeticos {
+    public static void main(String[] args) {
+    
+    
+    // Operadores Aritméticos
+    double soma = 5 + 3;
+    int subtracao = 10 - 2;
+    int multiplicacao = 4 * 4;
+    double divisao = 20.0 / 4.0;
+    int restoDaDivisao = 10 % 3;
+    double resultado = (2 + 3) * 4;
+    double resultado2 = (soma + subtracao) * multiplicacao / divisao;
+    
+
+    // Exibindo os resultados
+    System.out.println(soma);  
+    System.out.println(subtracao);
+    System.out.println(multiplicacao);
+    System.out.println(divisao);
+    System.out.println(restoDaDivisao);
+    System.out.println(resultado);
+    System.out.println(resultado2);
+    }
+}

df7c228deb8ee0c42e34f76e6db7ce9366f20e89
Leonardo Francisco Da Silva <leozinho24680@gmail.com>
2025-11-05 08:57:39 -0300

    exemplos de constantes, e tipos-primitivos

---
+++ sintaxe-java/src/edu/leonardo/tipos-Variaveis/constantes.java
@@ -0,0 +1,19 @@
+public class constantes {   
+    public static void main(String[] args) {
+        //Constantes em java:
+
+
+        //Uma constante é uma variavel que não pode ter seu valor alterado durante a execução do programa
+        //Para declarar uma constante em java utilizamos a palavra reservada final antes do tipo da variavel
+        //Por convenção, o nome das constantes é sempre em letras maiusculas
+        //Exemplo:
+        final double PI = 3.14;
+        final int ANO_NASCIMENTO = 2010;
+
+
+        //Exibindo os valores na tela
+        System.out.println(PI);
+        System.out.println(ANO_NASCIMENTO);
+    }
+    
+}
---
+++ sintaxe-java/src/edu/leonardo/tipos-Variaveis/tipos_primitivos.java
@@ -0,0 +1,32 @@
+public class tipos_primitivos {
+    public static void main(String[] args) {
+        
+        //Tipos primitivos em java:
+
+        byte idade = 15;
+        short ano = 2015;
+
+        int quantidade = 1500;
+        //Se INT comecar com 0 ele entende que é um numero octal, sendo assim o valor seria uma string nao um INT]
+        // Exemplo:
+        int cep = 01234567;
+
+        //Lomg sempre fecha sua variavel com L
+        //float sempre fecha sua variavel com F
+        //Exemplo:
+        long cpf = 123875790231L;
+        float pi = 3.14F;
+
+        double salario = 1235.50;
+
+        ///Exibindo os valores na tela
+
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(quantidade);
+        System.out.println(cep);
+        System.out.println(cpf);
+        System.out.println(pi);
+        System.out.println(salario);
+    }
+}

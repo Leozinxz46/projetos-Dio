4f268bdfb5edb0010e6773cbebda1101c365de96
Leonardo Francisco Da Silva <leozinho24680@gmail.com>
2025-11-05 16:45:13 -0300

    unitarios trilha de estudos dio

---
+++ sintaxe-java/src/edu/leonardo/operadores/unarios.java
@@ -0,0 +1,14 @@
+package edu.leonardo.operadores;
+
+public class unarios{
+    public static void main(String[] args) {
+        
+        int numero = -5;
+
+        //transforma o numero negativo em positivo
+        numero = numero * -1;
+
+        System.out.println(numero);
+    }
+    
+}
